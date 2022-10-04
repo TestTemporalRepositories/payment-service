f43d9b85918ed323521e8ac098077021d019e558
sotskov <vic.sotskov@gmail.com>
2022-10-04 11:39:34 +0700

    Сервис дополнен

--- src/main/java/org/payment/payment/config/WorkerFactoryStarter.java
+++ src/main/java/org/payment/payment/config/WorkerFactoryStarter.java
@@ -3,6 +3,7 @@ package org.payment.payment.config;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
 import lombok.RequiredArgsConstructor;
+import org.payment.payment.workflows.GreetingWorkflowImpl;
 import org.payment.payment.config.properties.WorkerProperties;
 import org.payment.payment.config.properties.Workers;
 import org.springframework.context.ApplicationListener;
@@ -18,12 +19,11 @@ public class WorkerFactoryStarter implements ApplicationListener<ContextRefreshe
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        setOrderWorker(workerPropertiesMap.get(Workers.PAYMENT.name()));
+        setPaymentServiceWorker(workerPropertiesMap.get(Workers.PAYMENT.name()));
         workerFactory.start();
     }
 
-    private void setOrderWorker(WorkerProperties workerProperties) {
+    private void setPaymentServiceWorker(WorkerProperties workerProperties) {
         Worker worker = workerFactory.newWorker(workerProperties.getQueueName());
-
     }
 }
--- src/main/java/org/payment/payment/persistance/repositories/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.payment.payment.persistance.repositories;
-
-import org.payment.payment.persistance.models.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-}
