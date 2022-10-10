d235d8c85001c5b3bf90827edd1bbc59ea141929
sotskov <vic.sotskov@gmail.com>
2022-10-10 13:50:32 +0700

    Сервис дополнен

--- src/main/java/org/payment/payment/rest/WorkflowRest.java
+++ src/main/java/org/payment/payment/rest/WorkflowRest.java
@@ -1,5 +1,7 @@
 package org.payment.payment.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.payment.payment.dto.PaymentDto;
 import org.payment.payment.services.PaymentService;
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Выполнение Workflow")
 public class WorkflowRest {
 
     private final PaymentService paymentService;
 
+
+    @Operation(
+            summary = "Новый перевод",
+            description = "Выполнение простого workflow CreatePaymentWorkflow с несколькими Activity"
+    )
     @PostMapping("payments")
     public ResponseEntity<String> test(
             @RequestBody PaymentDto paymentDto
@@ -25,6 +33,10 @@ public class WorkflowRest {
         );
     }
 
+    @Operation(
+            summary = "Новый перевод с вложенным Workflow",
+            description = "Выполнение workflow ParentCreatePaymentWorkflow с несколькими Activity и вложенным (child) Workflow"
+    )
     @PostMapping("payments-with-child-workflow")
     public ResponseEntity<String> testChild(
             @RequestBody PaymentDto paymentDto
@@ -35,6 +47,10 @@ public class WorkflowRest {
         );
     }
 
+    @Operation(
+            summary = "Новый перевод с версионированием",
+            description = "Выполнение workflow VerCreatePaymentWorkflow с использованием версий"
+    )
     @PostMapping("payments-with-versioning-workflow")
     public ResponseEntity<String> testVersion(
             @RequestBody PaymentDto paymentDto
